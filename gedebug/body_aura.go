f9d424cb4dea6cc2b958646962d1c0b95d8f203c
Iskander Sharipov <quasilyte@gmail.com>
2022-04-21 16:01:30 +0300

    add gedebug package
    gedebug package provides functions and types that are
    useful during the game debugging.

---
+++ gedebug/body_aura.go
@@ -0,0 +1,39 @@
+package gedebug
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/quasilyte/ge/collision"
+	"github.com/quasilyte/ge/gedraw"
+)
+
+type BodyAura struct {
+	Body *collision.Body
+}
+
+func (a *BodyAura) Draw(screen *ebiten.Image) {
+	c := color.RGBA{G: 100, B: 200, A: 100}
+
+	if a.Body.IsRect() {
+		rect := a.Body.BoundsRect()
+		ebitenutil.DrawRect(screen, float64(rect.X1()), float64(rect.Y1()), float64(rect.Width()), float64(rect.Height()), c)
+		return
+	}
+
+	if a.Body.IsRotatedRect() {
+		vertices := a.Body.RotatedRectVertices()
+		gedraw.DrawPath(screen, vertices[:], c)
+		return
+	}
+
+	if a.Body.IsCircle() {
+		gedraw.DrawCircle(screen, a.Body.Pos, a.Body.CircleRadius(), c)
+		return
+	}
+
+	panic("unsupported body kind")
+}
+
+func (a *BodyAura) IsDisposed() bool { return a.Body.IsDisposed() }
