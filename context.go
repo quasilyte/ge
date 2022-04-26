aa4c34a47956116cbc7790680f6c679ef06bfd33
Iskander Sharipov <quasilyte@gmail.com>
2022-04-26 20:43:35 +0300

    rework loader and audio systems

--- context.go
+++ context.go
@@ -2,15 +2,17 @@ package ge
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/quasilyte/ge/audio"
 	"github.com/quasilyte/ge/gemath"
+	"github.com/quasilyte/ge/loader"
 )
 
 type Context struct {
-	Loader   *Loader
+	Loader   *loader.Cache
 	Renderer *Renderer
 
 	Input Input
-	Audio Audio
+	Audio audio.System
 
 	Rand gemath.Rand
 
@@ -27,15 +29,15 @@ func NewContext() *Context {
 	ctx := &Context{
 		WindowTitle: "GE Game",
 	}
-	ctx.Loader = NewLoader(ctx)
+	ctx.Loader = loader.NewCache()
 	ctx.Renderer = NewRenderer()
 	ctx.Rand.SetSeed(0)
 	ctx.Input.init()
-	ctx.Audio.init(ctx)
+	ctx.Audio.Init(ctx.Loader)
 	ctx.OnCriticalError = func(err error) {
 		panic(err)
 	}
-	ctx.Loader.audio = &ctx.Audio
+	ctx.Loader.WAVDecoder = &ctx.Audio
 	return ctx
 }
 
@@ -52,9 +54,15 @@ func (ctx *Context) NewScene(name string, controller SceneController) *Scene {
 }
 
 func (ctx *Context) LoadSprite(path string) *Sprite {
-	return NewSprite(ctx.Loader.LoadImage(path))
+	return NewSprite(ctx.Loader.GetImage(path))
 }
 
 func (ctx *Context) Draw(screen *ebiten.Image) {
 	ctx.CurrentScene.graphics = ctx.Renderer.Draw(screen, ctx.CurrentScene.graphics)
+}
+
+func (ctx *Context) WindowRect() gemath.Rect {
+	return gemath.Rect{
+		Max: gemath.Vec{X: ctx.WindowWidth, Y: ctx.WindowHeight},
+	}
 }
