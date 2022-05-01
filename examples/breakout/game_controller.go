a2da2b7ef19a200112427b0c3bcb81ecd8e333ce
Iskander Sharipov <quasilyte@gmail.com>
2022-05-01 18:37:57 +0300

    examples/breakout: reorganize game controller methods

--- examples/breakout/game_controller.go
+++ examples/breakout/game_controller.go
@@ -16,58 +16,6 @@ func newGameController() *gameController {
 	return &gameController{}
 }
 
-func (c *gameController) initLevel(scene *ge.Scene) {
-	c.rotatingBricks.rotate = 1
-	c.slidingLeftBricks.dx = -16
-	c.slidingLeftBricks.slideTime = 4
-	c.slidingRightBricks.dx = 16
-	c.slidingRightBricks.slideTime = 4
-
-	{
-		pos := gemath.Vec{X: 544, Y: 64}
-		rotation := gemath.Rad(0)
-		for i := 0; i < 14; i++ {
-			br := newBrick(1, rotation)
-			br.body.Pos = pos
-			scene.AddObject(br)
-			rotation += 0.45
-			pos = pos.MoveInDirection(80, rotation)
-			c.rotatingBricks.bricks = append(c.rotatingBricks.bricks, br)
-		}
-	}
-	for i := 0; i < 5; i++ {
-		br := newBrick(1.2, 0)
-		x := 128 + 16
-		if i%2 == 0 {
-			x += 96
-			c.slidingLeftBricks.bricks = append(c.slidingLeftBricks.bricks, br)
-		} else {
-			c.slidingRightBricks.bricks = append(c.slidingRightBricks.bricks, br)
-		}
-		br.body.Pos = gemath.Vec{X: float64(x), Y: float64(i*96 + 64)}
-		scene.AddObject(br)
-	}
-
-	{
-		br := newCircleBrick(1)
-		br.body.Pos = gemath.Vec{X: 512, Y: 240}
-		scene.AddObject(br)
-
-		br = newCircleBrick(0.8)
-		br.body.Pos = gemath.Vec{X: 512 - 64, Y: 240 - 64}
-		scene.AddObject(br)
-		br = newCircleBrick(0.8)
-		br.body.Pos = gemath.Vec{X: 512 + 64, Y: 240 - 64}
-		scene.AddObject(br)
-		br = newCircleBrick(0.8)
-		br.body.Pos = gemath.Vec{X: 512 - 64, Y: 240 + 64}
-		scene.AddObject(br)
-		br = newCircleBrick(0.8)
-		br.body.Pos = gemath.Vec{X: 512 + 64, Y: 240 + 64}
-		scene.AddObject(br)
-	}
-}
-
 func (c *gameController) Init(scene *ge.Scene) {
 	ctx := scene.Context()
 
@@ -121,4 +69,56 @@ func (c *gameController) Update(delta float64) {
 	c.rotatingBricks.Update(delta)
 	c.slidingLeftBricks.Update(delta)
 	c.slidingRightBricks.Update(delta)
+}
+
+func (c *gameController) initLevel(scene *ge.Scene) {
+	c.rotatingBricks.rotate = 1
+	c.slidingLeftBricks.dx = -16
+	c.slidingLeftBricks.slideTime = 4
+	c.slidingRightBricks.dx = 16
+	c.slidingRightBricks.slideTime = 4
+
+	{
+		pos := gemath.Vec{X: 544, Y: 64}
+		rotation := gemath.Rad(0)
+		for i := 0; i < 14; i++ {
+			br := newBrick(1, rotation)
+			br.body.Pos = pos
+			scene.AddObject(br)
+			rotation += 0.45
+			pos = pos.MoveInDirection(80, rotation)
+			c.rotatingBricks.bricks = append(c.rotatingBricks.bricks, br)
+		}
+	}
+	for i := 0; i < 5; i++ {
+		br := newBrick(1.2, 0)
+		x := 128 + 16
+		if i%2 == 0 {
+			x += 96
+			c.slidingLeftBricks.bricks = append(c.slidingLeftBricks.bricks, br)
+		} else {
+			c.slidingRightBricks.bricks = append(c.slidingRightBricks.bricks, br)
+		}
+		br.body.Pos = gemath.Vec{X: float64(x), Y: float64(i*96 + 64)}
+		scene.AddObject(br)
+	}
+
+	{
+		br := newCircleBrick(1)
+		br.body.Pos = gemath.Vec{X: 512, Y: 240}
+		scene.AddObject(br)
+
+		br = newCircleBrick(0.8)
+		br.body.Pos = gemath.Vec{X: 512 - 64, Y: 240 - 64}
+		scene.AddObject(br)
+		br = newCircleBrick(0.8)
+		br.body.Pos = gemath.Vec{X: 512 + 64, Y: 240 - 64}
+		scene.AddObject(br)
+		br = newCircleBrick(0.8)
+		br.body.Pos = gemath.Vec{X: 512 - 64, Y: 240 + 64}
+		scene.AddObject(br)
+		br = newCircleBrick(0.8)
+		br.body.Pos = gemath.Vec{X: 512 + 64, Y: 240 + 64}
+		scene.AddObject(br)
+	}
 }
