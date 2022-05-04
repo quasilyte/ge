498146248769e4a2903d3732fbf0c7b5d7f53873
Iskander Sharipov <quasilyte@gmail.com>
2022-05-04 10:05:45 +0300

    gemath: add rand.Chance method

--- gemath/rand.go
+++ gemath/rand.go
@@ -14,12 +14,16 @@ func (r *Rand) SetSeed(seed int64) {
 	r.rng = rand.New(src)
 }
 
+func (r *Rand) Chance(probability float64) bool {
+	return r.rng.Float64() <= probability
+}
+
 func (r *Rand) IntRange(min, max int) int {
 	return min + r.rng.Intn(max-min+1)
 }
 
 func (r *Rand) FloatRange(min, max float64) float64 {
-	return min + r.rng.Float64()*(max-min)
+	return min + r.rng.Float64()*(max-min+1)
 }
 
 func (r *Rand) Rad() Rad {
