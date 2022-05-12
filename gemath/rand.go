5dccf2b0a3bd1a228c0771f68f2cedd5567d2781
Iskander Sharipov <quasilyte@gmail.com>
2022-05-12 09:31:27 +0300

    gemath: add more rand util methods

--- gemath/rand.go
+++ gemath/rand.go
@@ -18,8 +18,16 @@ func (r *Rand) Chance(probability float64) bool {
 	return r.rng.Float64() <= probability
 }
 
+func (r *Rand) Bool() bool {
+	return r.rng.Float64() < 0.5
+}
+
 func (r *Rand) IntRange(min, max int) int {
 	return min + r.rng.Intn(max-min+1)
+}
+
+func (r *Rand) Float() float64 {
+	return r.rng.Float64()
 }
 
 func (r *Rand) FloatRange(min, max float64) float64 {
@@ -28,4 +36,21 @@ func (r *Rand) FloatRange(min, max float64) float64 {
 
 func (r *Rand) Rad() Rad {
 	return Rad(r.FloatRange(0, 2*math.Pi))
+}
+
+func RandIndex[T any](r *Rand, slice []T) int {
+	if len(slice) == 0 {
+		return -1
+	}
+	return r.IntRange(0, len(slice)-1)
+}
+
+func RandElem[T any](r *Rand, slice []T) (elem T) {
+	if len(slice) == 0 {
+		return elem // Zero value
+	}
+	if len(slice) == 1 {
+		return slice[0]
+	}
+	return slice[r.rng.Intn(len(slice))]
 }
