80a949ee9c8fc86a7d3017c5efb8cb4301696822
Iskander Sharipov <quasilyte@gmail.com>
2022-06-07 00:57:55 +0300

    gemath: add ArcContains utility function

--- gemath/utils.go
+++ gemath/utils.go
@@ -1,11 +1,29 @@
 package gemath
 
-import "math"
+import (
+	"math"
+)
 
 const Epsilon = 1e-9
 
 func EqualApprox(a, b float64) bool {
 	return math.Abs(a-b) <= Epsilon
+}
+
+func ArcContains(angle, measure Rad, r float64, pos, point Vec) bool {
+	if pos.DistanceSquaredTo(point) > r*r {
+		return false
+	}
+	startAngle := (angle - measure/2)
+	endAngle := (angle + measure/2)
+	if endAngle < startAngle {
+		endAngle += 2 * math.Pi
+	}
+	half := (endAngle - startAngle) / 2
+	mid := (endAngle + startAngle) / 2
+	coshalf := math.Cos(float64(half))
+	angleToPoint := pos.AngleToPoint(point).Normalized()
+	return math.Cos(float64(angleToPoint-mid)) >= coshalf
 }
 
 func Clamp[T numeric](v, min, max T) T {
