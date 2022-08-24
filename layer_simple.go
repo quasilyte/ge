e8707f6541223676d0087b30151a7726b88bf66f
Iskander Sharipov <quasilyte@gmail.com>
2022-08-25 01:45:58 +0300

    add SimpleLayer

---
+++ layer_simple.go
@@ -0,0 +1,47 @@
+package ge
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type SimpleLayer struct {
+	Visible  bool
+	disposed bool
+
+	graphics []SceneGraphics
+}
+
+func NewSimpleLayer() *SimpleLayer {
+	return &SimpleLayer{
+		Visible:  true,
+		graphics: make([]SceneGraphics, 0, 8),
+	}
+}
+
+func (l *SimpleLayer) AddGraphics(g SceneGraphics) {
+	l.graphics = append(l.graphics, g)
+}
+
+func (l *SimpleLayer) IsDisposed() bool {
+	return l.disposed
+}
+
+func (l *SimpleLayer) Dispose() {
+	l.disposed = true
+}
+
+func (l *SimpleLayer) Draw(screen *ebiten.Image) {
+	if !l.Visible {
+		return
+	}
+
+	list := l.graphics[:0]
+	for _, g := range l.graphics {
+		if g.IsDisposed() {
+			continue
+		}
+		g.Draw(screen)
+		list = append(list, g)
+	}
+	l.graphics = list
+}
